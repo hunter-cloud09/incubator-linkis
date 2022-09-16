dc5a50b774ba7eaf84f5478bd777251a49e53a43
Jack Xu <xulingcom@126.com>
2022-09-16 16:52:28 +0800

    fix: code format

--- linkis-public-enhancements/linkis-datasource/linkis-metadata/src/main/scala/org/apache/linkis/metadata/ddl/ScalaDDLCreator.scala
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata/src/main/scala/org/apache/linkis/metadata/ddl/ScalaDDLCreator.scala
@@ -100,7 +100,9 @@ object ScalaDDLCreator extends DDLCreator with SQLConst with Logging {
         val strings: Array[String] = tblPropInfo.split(COMMA)
         strings.foreach(kv => {
           val strings1: Array[String] = kv.split(EQ)
-          tabPropArray += (SINGLE_MARK + strings1.apply(0) + SINGLE_MARK + EQ + SINGLE_MARK + strings1
+          tabPropArray += (SINGLE_MARK + strings1.apply(
+            0
+          ) + SINGLE_MARK + EQ + SINGLE_MARK + strings1
             .apply(1) + SINGLE_MARK)
         })
       }
